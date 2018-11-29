18c86ab4ac5c5b0410376465bc406e9d5d47c543
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2018-11-29 17:14:30 -0500

    filter per last digit of the license plate number

--- objectCar.py
+++ objectCar.py
@@ -6,26 +6,33 @@ class Car:
     __afternMinhour = time(16)
     __afternMaxhour = time(19,30)
 
-    def __init__(self,placa,date_str,time_str):
+    def __init__(self,plate_str,date_str,time_str):
         self.dateAtrib = date_str
         self.timeAtrib = time_str
+        self.plateAtrib = plate_str
         pass
 
     def canbeontheRoad(self):
 
         #filter per day of the week
-        allowedDay = True
+        #the day of the week are Monday=1, Tuesday=2 .... Sunday=7 for this program
         compareDate = datetime.strptime(self.dateAtrib, '%Y-%m-%d').date()
         dayofWeek = (compareDate.toordinal()-1)%7+1
-        if dayofWeek<6:
-            allowedDay = False
+        if dayofWeek>5:
+            return True
         
         #filter per hour
-        allowedHour = True
         compareTime = datetime.strptime(self.timeAtrib,"%X").time()
         rangeMorning = (compareTime >= self.__mornMinhour) and (compareTime <= self.__mornMaxhour)
         rangeAfternoon = (compareTime >= self.__afternMinhour) and (compareTime <= self.__afternMaxhour)
-        if (rangeMorning or rangeAfternoon):
-            allowedHour = False
-
-        return (allowedDay or allowedHour)
+        if not(rangeMorning or rangeAfternoon):
+            return True
+        
+        #filter per last digit of the license plate number
+        # dictionary structure, dayandNumber={number_of_week:[last digit]}
+        lastDigit = int(self.plateAtrib[-1])
+        dayandNumber = {1:[1,2],2:[3,4],3:[5,6],4:[7,8],5:[9,0]}
+        if not(lastDigit in dayandNumber.get(dayofWeek)):
+            return True
+        
+        return False
--- test_objectCar_canbeontheroad.py
+++ test_objectCar_canbeontheroad.py
@@ -8,20 +8,28 @@ class canroadTestCase(unittest.TestCase):
         car1=Car('ABC-1234',date_str,'07:20:11')
         self.assertEqual(car1.canbeontheRoad(),True)
     
-    #Test. It is not Weekend and it is between the lag of hours allowed
+    #Test. It is not Weekend and between the lag of hours are allowed
     def test_canbeontheRoad_allowedHour(self):
         date_str='2018-11-29'
         time_str='19:30:01'
         car1=Car('ABC-1234',date_str,time_str)
         self.assertEqual(car1.canbeontheRoad(),True)
 
-    #Test. It is not Weekend and it is between the lag of hours not allowed
-    def test_canbeontheRoad_notallowedHour(self):
-        date_str='2018-11-29'
+    #Test. It is not Weekend , between the lag of hours  and last digit number are allowed
+    def test_canbeontheRoad_allowedPlate(self):
+        date_str='2018-11-27'
         time_str='7:00:00'
-        car1=Car('ABC-1234',date_str,time_str)
-        self.assertEqual(car1.canbeontheRoad(),False)
+        licPlatenum='ABC-1231'
+        car1=Car(licPlatenum,date_str,time_str)
+        self.assertEqual(car1.canbeontheRoad(),True)
     
+    #Test. It is not Weekend, between the lag of hours and last digit number aren't allowed
+    def test_canbeontheRoad_notallowedPlate(self):
+        date_str='2018-11-26'
+        time_str='7:00:00'
+        licPlatenum='ABC-1231'
+        car1=Car(licPlatenum,date_str,time_str)
+        self.assertEqual(car1.canbeontheRoad(),False)
     
 if __name__=='__main__':
     unittest.main()
