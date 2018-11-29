9e3a99868b1290fc60cebda5de92b7a0c87de2d9
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2018-11-29 16:08:38 -0500

    Filter per hour

--- objectCar.py
+++ objectCar.py
@@ -1,17 +1,31 @@
-from datetime import datetime
+from datetime import datetime,time
 
 class Car:
-    def __init__(self,placa,date,hora):
-        self.date_str = date
+    __mornMinhour = time(7)
+    __mornMaxhour = time(9,30)
+    __afternMinhour = time(16)
+    __afternMaxhour = time(19,30)
+
+    def __init__(self,placa,date_str,time_str):
+        self.dateAtrib = date_str
+        self.timeAtrib = time_str
         pass
+
     def canbeontheRoad(self):
 
         #filter per day of the week
-        allowedDay=True
-        compare_date = datetime.strptime(self.date_str, '%Y-%m-%d').date()
-        dayofWeek = (compare_date.toordinal()-1)%7+1
+        allowedDay = True
+        compareDate = datetime.strptime(self.dateAtrib, '%Y-%m-%d').date()
+        dayofWeek = (compareDate.toordinal()-1)%7+1
         if dayofWeek<6:
-            allowedDay=False
+            allowedDay = False
         
+        #filter per hour
+        allowedHour = True
+        compareTime = datetime.strptime(self.timeAtrib,"%X").time()
+        rangeMorning = (compareTime >= self.__mornMinhour) and (compareTime <= self.__mornMaxhour)
+        rangeAfternoon = (compareTime >= self.__afternMinhour) and (compareTime <= self.__afternMaxhour)
+        if (rangeMorning or rangeAfternoon):
+            allowedHour = False
 
-        return allowedDay
+        return (allowedDay or allowedHour)
--- test_objectCar_canbeontheroad.py
+++ test_objectCar_canbeontheroad.py
@@ -2,17 +2,26 @@ import unittest
 from objectCar import Car
 
 class canroadTestCase(unittest.TestCase): 
-    #Test it is Weekend
+    #Test. It is Weekend
     def test_canbeontheRoad_allowedDay(self):
         date_str='2018-11-24'
         car1=Car('ABC-1234',date_str,'07:20:11')
         self.assertEqual(car1.canbeontheRoad(),True)
-
-    #Test it is not Weekend
-    def test_canbeontheRoad_notallowedDay(self):
+    
+    #Test. It is not Weekend and it is between the lag of hours allowed
+    def test_canbeontheRoad_allowedHour(self):
         date_str='2018-11-29'
-        car1=Car('ABC-1234',date_str,'10:20:11')
+        time_str='19:30:01'
+        car1=Car('ABC-1234',date_str,time_str)
+        self.assertEqual(car1.canbeontheRoad(),True)
+
+    #Test. It is not Weekend and it is between the lag of hours not allowed
+    def test_canbeontheRoad_notallowedHour(self):
+        date_str='2018-11-29'
+        time_str='7:00:00'
+        car1=Car('ABC-1234',date_str,time_str)
         self.assertEqual(car1.canbeontheRoad(),False)
+    
     
 if __name__=='__main__':
     unittest.main()
